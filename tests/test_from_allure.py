e6cdeafa59ac89c13e10cbc65dc94e1543be7213
Bezgubenko Helen <protasova-ea@yandex.ru>
2023-10-09 14:44:16 +0300

    add manual case from allure

---
+++ tests/test_from_allure.py
@@ -0,0 +1,18 @@
+import allure
+
+
+@allure.id("26553")
+@allure.title("Manual test_create_user")
+@allure.label("suite", "test_regres")
+@allure.label("owner", "allure8")
+def test_login_ui():
+    with allure.step("Open page login"):
+        pass
+    with allure.step("Fill user_name"):
+        pass
+    with allure.step("Fill password"):
+        pass
+    with allure.step("Submit"):
+        pass
+    with allure.step("Check success login"):
+        pass
